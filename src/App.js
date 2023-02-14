c12d20a67f3f2993561110bad35296474f1555ce
bavila19 <brendaavila015@gmail.com>
2023-02-14 11:15:10 -0700

    Added NavBar and imported Anchor link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import NavBar from "./scenes/NavBar"
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+
 
 function App() {
+  const [ selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
-    <div className="App">
+    <div className="app bg-deep-blue">
+      <NavBar selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}/>
      
     </div>
   );
