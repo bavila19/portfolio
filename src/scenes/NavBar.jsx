3693feab40ce168bdb20393ab5ea90e4edc1df2e
bavila19 <brendaavila015@gmail.com>
2023-02-14 11:15:19 -0700

    Added NavBar and imported Anchor link

--- src/scenes/NavBar.jsx
+++ src/scenes/NavBar.jsx
@@ -5,4 +5,10 @@ import useMediaQuery from "../hooks/useMediaQuery";
 const NavBar = ({selectedPage, setSelectPage}) =>{
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+
+    return (
+        <nav className={`z-40 w-full fixed top-0 py-6`}>
+
+        </nav>
+    )
 }
