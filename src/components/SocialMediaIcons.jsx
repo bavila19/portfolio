957ebef735ab1b35cc2b2c988d003ba8f6681e05
bavila19 <brendaavila015@gmail.com>
2023-02-21 14:20:49 -0700

    Added Landing file and icons

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,27 @@
+import linkedin from "../assets/linkedin.svg"
+import github from "../assets/github.svg"
+
+const SocialMediaIcons = ()=> {
+    return(
+        <div className=" flex justify-center md:justify-start my-10 gap-7">
+            <a 
+                className="hover:opacity-50 transition duration-500 w-[45px]"
+                href="https://www.linkedin.com/in/brenda-avila-escamilla/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="linkedin-link" src={linkedin}/>
+            </a>
+            <a 
+                className="hover:opacity-50 transition duration-500 w-[45px]"
+                href="https://www.linkedin.com/in/brenda-avila-escamilla/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="github-link" src={github}/>
+            </a>
+        </div>
+    )
+
+}
+export default SocialMediaIcons
