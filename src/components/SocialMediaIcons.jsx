2d61f3d671bfe0db4b1b837bf789aafd120ae967
bavila19 <brendaavila015@gmail.com>
2023-03-01 12:46:18 -0700

    just a git commit

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,5 +1,6 @@
 import linkedin from "../assets/linkedin.svg"
 import github from "../assets/github.svg"
+import resume from "../assets/resume.png"
 
 const SocialMediaIcons = ()=> {
     return(
@@ -19,6 +20,14 @@ const SocialMediaIcons = ()=> {
                 rel="noreferrer"
             >
                 <img alt="github-link" src={github}/>
+            </a>
+            <a 
+                className="hover:opacity-50 transition duration-500 w-[45px]"
+                href="https://www.linkedin.com/in/brenda-avila-escamilla/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="resume-link" src={resume}/>
             </a>
         </div>
     )
--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -1,5 +1,38 @@
 import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
+import trending from "../assets/trending.png"
+import rootedEarth from "../assets/rootedEarth.png"
+import feeling from "../assets/feeling.png"
+// import projectTitle from `../assets/${projectTitle}.png`
+// let projectTitle = await import ("../assets/")
+
+ 
+const container = {
+    hidden: {},
+    visible:{
+        transition: {staggerChildren:0.2}
+    }
+};
+
+const projectVariant = {
+    hidden:{opacity:0, scale:0.8},
+    visible: {opacity: 1, scale:1 }
+}
+
+const Project =({title, subtitle})=>{
+    const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition  duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+    // const projectTitle = title.pslit(" ").join("-").toLowerCase();
+
+    return(
+        <motion.div variants={projectVariant} className="relative">
+            <div className={overlayStyles}>
+                <p className="text-2xl font-playfair">{title}</p>
+                <p>{subtitle}</p>
+            </div>
+            {/* <img src={projectTitle} alt= "ahh"/> */}
+        </motion.div>
+    )
+}
 
 const Projects = () => {
   return (
@@ -24,10 +57,38 @@ const Projects = () => {
             <LineGradient width="w=1/3" />
           </div>
         </div>
-        <p className="mt-10 mb-7">
-        These software engineering projects were completed within a one-week sprint and served as valuable opportunities for professional growth. By collaborating with other software engineers, I were able to develop teamwork and communication skills while also learning new techniques and approaches. I had the chance to experiment with different frameworks, broadening my knowledge and expertise. Additionally, managing my own time during these projects has honed my organizational skills and enabled you to work more efficiently.        
+        <p className="mt-10 mb-10">
+          These software engineering projects were completed within a one-week
+          sprint and served as valuable opportunities for professional growth.
+          By collaborating with other software engineers, I were able to develop
+          teamwork and communication skills while also learning new techniques
+          and approaches. I had the chance to experiment with different
+          frameworks, broadening my knowledge and expertise. Additionally,
+          managing my own time during these projects has honed my organizational
+          skills and enabled you to work more efficiently.
         </p>
       </motion.div>
+      {/* PROJECTS */}
+      <div className="flex justify-center">
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+            {/* ROW ONE */}
+            <div className="flex justify-center text-center items-center p-10 bg-red first-letter max-w-[400px] text-2xl font-playfair font-semibold">
+                BEUT
+            </div>
+            <Project title=" Rooted Earth" 
+            subtitle={"hello"}
+/>
+            <Project title="project 1" subtitle={"hello"}><img src={rootedEarth} alt="work"/></Project>
+            <Project title="project 1" subtitle={"hello"}/>
+
+        </motion.div>
+      </div>
     </section>
   );
 };
